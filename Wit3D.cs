e2341c0b59c09050add9f05a98f95733391662c6
Longchang Cui <cuilongchang@hotmail.com>
2020-05-08 14:50:32 -0400

    Update Wit3D.cs

--- Wit3D.cs
+++ Wit3D.cs
@@ -13,78 +13,76 @@ using System.Text;
 
 public partial class Wit3D : MonoBehaviour {
 
-	// Class Variables
+    // Class Variables
 
-	// Audio variables
-	public AudioClip commandClip;
-	int samplerate;
+    // Audio variables
+    public AudioClip commandClip;
+    int samplerate;
 
-	// API access parameters
-	string url;
-	string token;
-	UnityWebRequest wr;
+    // API access parameters
+    string url;
+    string token;
+    UnityWebRequest wr;
 
-	// Movement variables
-	public float moveTime;
-	public float yOffset;
+    // Movement variables
+    public float moveTime;
+    public float yOffset;
 
-	// GameObject to use as a default spawn point
-	public GameObject spawnPoint;
+    // GameObject to use as a default spawn point
+    public GameObject spawnPoint;
     
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 
-		// If you are a Windows user and receiving a Tlserror
-		// See: https://github.com/afauch/wit3d/issues/2
-		// Uncomment the line below to bypass SSL
-	    System.Net.ServicePointManager.ServerCertificateValidationCallback = (a, b, c, d) => { return true; };
+	// Uncomment the line below to bypass SSL
+        System.Net.ServicePointManager.ServerCertificateValidationCallback = (a, b, c, d) => { return true; };
 
-		// set samplerate to 16000 for wit.ai
-		samplerate = 16000;
+	// set samplerate to 16000 for wit.ai
+	samplerate = 16000;
 
-	}
+    }
 
-	string GetJSONText(string file) {
+    string GetJSONText(string file) {
 
-		// get the file w/ FileStream
+	// get the file w/ FileStream
 
-		FileStream filestream = new FileStream (file, FileMode.Open, FileAccess.Read);
-		BinaryReader filereader = new BinaryReader (filestream);
-        	byte[] BA_AudioFile = filereader.ReadBytes ((Int32)filestream.Length);
-        	filestream.Close ();
-		filereader.Close ();
+	FileStream filestream = new FileStream (file, FileMode.Open, FileAccess.Read);
+	BinaryReader filereader = new BinaryReader (filestream);
+	byte[] BA_AudioFile = filereader.ReadBytes ((Int32)filestream.Length);
+	filestream.Close ();
+	filereader.Close ();
 
-		// create an HttpWebRequest
-		HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://api.wit.ai/speech");
-        
-		request.Method = "POST";
-			request.Headers ["Authorization"] = "Bearer " + token;
-			request.ContentType = "audio/wav";
-			request.ContentLength = BA_AudioFile.Length;
-		request.GetRequestStream ().Write (BA_AudioFile, 0, BA_AudioFile.Length);
-        
-		// Process the wit.ai response
-		try
-			{
-				HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-				if (response.StatusCode == HttpStatusCode.OK)
-				{
-					print("Http went through ok");
-					StreamReader response_stream = new StreamReader(response.GetResponseStream());
-					return response_stream.ReadToEnd();
-				}
-				else
-				{
-					return "Error: " + response.StatusCode.ToString();
-					return "HTTP ERROR";
-				}
-			}
-			catch (Exception ex)
-			{
-				return "Error: " + ex.Message;
-				return "HTTP ERROR";
-			}    
+	// create an HttpWebRequest
+	HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://api.wit.ai/speech");
+
+	request.Method = "POST";
+		request.Headers ["Authorization"] = "Bearer " + token;
+		request.ContentType = "audio/wav";
+		request.ContentLength = BA_AudioFile.Length;
+	request.GetRequestStream ().Write (BA_AudioFile, 0, BA_AudioFile.Length);
+
+        // Process the wit.ai response
+        try
+	    {
+		HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+		if (response.StatusCode == HttpStatusCode.OK)
+		{
+			print("Http went through ok");
+			StreamReader response_stream = new StreamReader(response.GetResponseStream());
+			return response_stream.ReadToEnd();
 		}
+		else
+		{
+			return "Error: " + response.StatusCode.ToString();
+			return "HTTP ERROR";
+		}
+	    }
+        catch (Exception ex)
+        {
+	    return "Error: " + ex.Message;
+	    return "HTTP ERROR";
+    	}    
+    }
     
     public void RecordingForWit()
     {
